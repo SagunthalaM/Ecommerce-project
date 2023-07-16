87ca80462d4239b44d307d754236432c9f7bb4c2
SagunthalaM <sagumadhaiyan@gmail.com>
2023-07-16 18:19:38 +0530

    localization commit

--- routes/web.php
+++ routes/web.php
@@ -58,29 +58,6 @@ Route::view('/','index')->middleware('guest')->name('index');
 //Route::view('index','index')->middleware('guest')->name('index');
 
 
-        Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function (){  
-        Route::get('/products',[AdminController::class, 'adminGetAllProducts'])->name('admin.products');
-        Route::delete('/products/{id}',[AdminController::class,'adminDeleteProduct'])->name('admin.products.delete');
-        Route::get('/products/create',[ProductController::class,'create'])->name('products.create');
-        Route::post('/products/store',[ProductController::class,'store'])->name('products.store');
-        Route::get('/products/edit/{id}',[ProductController::class,'edit'])->name('products.edit');
-        Route::post('/products/update/{id}',[ProductController::class,'update'])->name('products.update');
-        Route::get('/users',[UserController::class,'index']);
-        Route::get('/add-user',[App\Http\Controllers\Admin\UserController::class,'AddUserIndex'])->name('AddUserIndex');
-        Route::post('/insert-user',[App\Http\Controllers\Admin\UserController::class,'InsertUser'])->name('InsertUser');
-        Route::get('/edit-user/{id}',[App\Http\Controllers\Admin\UserController::class,'EditUser'])->name('Edituser');
-        Route::post('/update-user/{id}',[App\Http\Controllers\Admin\UserController::class,'UpdateUser'])->name('UpdateUser');
-        Route::get('/delete-user/{id}',[App\Http\Controllers\Admin\UserController::class,'DeleteUser'])->name('Deleteuser');
-        });
-
-        Route::get('/products',[ProductController::class,'index'])->name('products.index')->middleware('auth');
-        Route::get('/products/{id}',[ProductController::class,'show'])->name('products.show');
-
-        //Authentication for login and register
-
-        Route::view('/','index')->middleware('guest')->name('index');
-        //Route::view('index','index')->middleware('guest')->name('index');
-
 Route::post('store',[RegisterController::class,'store'])->middleware('guest');
 
 Route::view('home','home')->name('home')->middleware(['auth','isAdmin']);
@@ -100,26 +77,6 @@ Route::get('myorders',[ProductController::class,'myOrders'])->middleware('auth')
 Route::get('totalorders',[AdminController::class,'totalOrders'])->name('admin.totalorders')->middleware(['auth','isAdmin']);
 Route::view('noitem','product.noitem')->middleware('auth');
 
-        Route::view('register','Auth.register')->middleware('guest');
-
-        Route::post('store',[RegisterController::class,'store']);
-
-       // Route::view('home','home')->middleware('auth');
-
-        Route::view('login','Auth.login')->middleware('guest')->name('login');
-
-        Route::post('authenticate',[LoginController::class,'authenticate']);
-
-        Route::get('logout',[LoginController::class,'logout']);
-
-        //Product and order details
-        Route::post('/add_to_cart',[ProductController::class,'addToCart'])->middleware('auth');
-        Route::get('/cartlist',[ProductController::class,'cartList'])->middleware('auth');
-        Route::get('removecart/{id}',[ProductController::class,'removeCart'])->middleware('auth');
-        Route::get('ordernow',[ProductController::class,'orderNow'])->middleware('auth');
-        Route::post('orderplace',[ProductController::class,'orderPlace']);
-        Route::get('myorders',[ProductController::class,'myOrders'])->middleware('auth');
-        Route::get('totalorders',[AdminController::class,'totalOrders'])->middleware('auth');
 
 
 //});
